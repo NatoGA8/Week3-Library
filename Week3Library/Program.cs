6af615aa270261f90c365317d893e54d41a2f40b
NatoGA8 <Nathangallagher16@gmail.com>
2024-10-09 00:44:45 +0100

    Initial commi

--- Week3Library/Program.cs
+++ Week3Library/Program.cs
@@ -1,13 +1,2 @@
-﻿using Library;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
-        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
-        book.DisplayInfo();
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
