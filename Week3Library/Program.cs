71d81e6e1b55eb23b988e566809068a98f67e0de
NatoGA8 <Nathangallagher16@gmail.com>
2024-10-09 01:00:38 +0100

    Access modifier

---
+++ Week3Library/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance (object) of the Book class
+        // Note how the object name differs from the class name
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+
+        book.DisplayInfo();
+    }
+}
